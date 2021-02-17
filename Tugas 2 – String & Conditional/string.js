e27c207effd1455a0721cdb75b06c59e3ebb742c
ehsantrisaputra <ehsantrisaputra@gmail.com>
2021-02-17 09:13:18 +0700

    my commit

--- Tugas 2 – String & Conditional/string.js
+++ Tugas 2 – String & Conditional/string.js
@@ -20,12 +20,12 @@ var sentence = "I am going to be React Native Developer";
 
 var firstWord = sentence[0] ; 
 var secondWord = sentence[2] + sentence[3]  ; 
-var thirdWord; = sentence[5] + sentence[9]  ; 
-var fourthWord; = sentence[11] + sentence[12]  ;  
-var fifthWord; = sentence[14] + sentence[15]  ; 
-var sixthWord; = sentence[2] + sentence[3]  ; 
-var seventhWord; = sentence[2] + sentence[3]  ; 
-var eighthWord; = sentence[2] + sentence[3]  ;  
+var thirdWord = sentence[5] + sentence[6] + sentence[7] + sentence[8] + sentence[9]  ; 
+var fourthWord = sentence[11] + sentence[12]  ;  
+var fifthWord = sentence[14] + sentence[15]  ; 
+var sixthWord = sentence[17] + sentence[18] + sentence[19] + sentence[20] + sentence[21]  ; 
+var seventhWord = sentence[23] + sentence[24] + sentence[25]+ sentence[26]+ sentence[27]+ sentence[28] ; 
+var eighthWord = sentence[30] + sentence[31] + sentence[32]+ sentence[33]+ sentence[34]+ sentence[35]+ sentence[36]+ sentence[37] + sentence[38] ;  
 
 console.log('First Word: ' + firstWord); 
 console.log('Second Word: ' + secondWord); 
@@ -37,11 +37,47 @@ console.log('Seventh Word: ' + seventhWord);
 console.log('Eighth Word: ' + eighthWord)
 
 
-First word: I 
-Second word: am 
-Third word: going 
-Fourth word: to 
-Fifth word: be 
-Sixth word: React 
-Seventh word: Native 
-Eighth word: Developer
+console.log('\n')
+
+
+console.log('No.3')
+
+var sentence2 = 'wow JavaScript is so cool'; 
+
+var exampleFirstWord2 = sentence2.substring(0, 3); 
+var secondWord2 = sentence2.substring(4, 14); 
+var thirdWord2 = sentence2.substring(15, 17); 
+var fourthWord2 = sentence2.substring(18, 20); 
+var fifthWord2 = sentence2.substring(21, 25);  
+
+console.log('First Word: ' + exampleFirstWord2); 
+console.log('Second Word: ' + secondWord2); 
+console.log('Third Word: ' + thirdWord2); 
+console.log('Fourth Word: ' + fourthWord2); 
+console.log('Fifth Word: ' + fifthWord2);
+
+console.log('\n')
+
+
+console.log('No.4')
+
+var sentence3 = 'wow JavaScript is so cool'; 
+
+var exampleFirstWord3 = sentence3.substring(0, 3); 
+var secondWord3 = sentence3.substring(4, 14); 
+var thirdWord3 = sentence3.substring(15, 17); 
+var fourthWord3 = sentence3.substring(18, 20); 
+var fifthWord3 = sentence3.substring(21, 25); 
+
+var firstWordLength = exampleFirstWord3.length;
+var secondWordLength = secondWord3.length; 
+var thirdWordLength = thirdWord3.length; 
+var fourthWordLength = fourthWord3.length; 
+var fifthWordLength = fifthWord3.length; 
+
+
+console.log('First Word: ' + exampleFirstWord3 + ', with length: ' + firstWordLength); 
+console.log('Second Word: ' + secondWord3 + ', with length: ' + secondWordLength);
+console.log('Third Word: ' + thirdWord3 + ', with length: ' + thirdWordLength);
+console.log('Fourth Word: ' + fourthWord3 + ', with length: ' + fourthWordLength);
+console.log('Fifth Word: ' + fifthWord3 + ', with length: ' + fifthWordLength);
