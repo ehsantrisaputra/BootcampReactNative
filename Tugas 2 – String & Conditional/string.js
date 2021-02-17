5a969a60d8f77a21700cdd35ba1721c376b7aa54
ehsantrisaputra <ehsantrisaputra@gmail.com>
2021-02-17 08:47:09 +0700

    first commit

---
+++ Tugas 2 – String & Conditional/string.js
@@ -0,0 +1,47 @@
+console.log("No. 1")
+
+var word = 'JavaScript'; 
+var second = 'is'; 
+var third = 'awesome'; 
+var fourth = 'and'; 
+var fifth = 'I'; 
+var sixth = 'love'; 
+var seventh = 'it!';
+
+console.log( word + " " + second + " " + third + " " + fourth + " " + fifth + " " + sixth + " " + seventh)
+
+console.log("\n")
+
+
+
+console.log("No. 2")
+
+var sentence = "I am going to be React Native Developer"; 
+
+var firstWord = sentence[0] ; 
+var secondWord = sentence[2] + sentence[3]  ; 
+var thirdWord; = sentence[5] + sentence[9]  ; 
+var fourthWord; = sentence[11] + sentence[12]  ;  
+var fifthWord; = sentence[14] + sentence[15]  ; 
+var sixthWord; = sentence[2] + sentence[3]  ; 
+var seventhWord; = sentence[2] + sentence[3]  ; 
+var eighthWord; = sentence[2] + sentence[3]  ;  
+
+console.log('First Word: ' + firstWord); 
+console.log('Second Word: ' + secondWord); 
+console.log('Third Word: ' + thirdWord); 
+console.log('Fourth Word: ' + fourthWord); 
+console.log('Fifth Word: ' + fifthWord); 
+console.log('Sixth Word: ' + sixthWord); 
+console.log('Seventh Word: ' + seventhWord); 
+console.log('Eighth Word: ' + eighthWord)
+
+
+First word: I 
+Second word: am 
+Third word: going 
+Fourth word: to 
+Fifth word: be 
+Sixth word: React 
+Seventh word: Native 
+Eighth word: Developer
